e5ab93a7c6624d613704585dd0ea43130e90e61c
krithiskkv <krithiskkv@gmail.com>
2016-09-27 10:25:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,8 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+
 function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
