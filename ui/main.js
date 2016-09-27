3722a2c4b180fddbfbbba7b9b9925164ad411426
krithiskkv <krithiskkv@gmail.com>
2016-09-27 10:45:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
-var img = document.getElementById('madi');
+var img = document.getElementById('prof');
 var marginLeft = 0;
 
 function moveRight() {
