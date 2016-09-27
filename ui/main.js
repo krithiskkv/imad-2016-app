99b24096453fcd99a5b286a2257dfe8ed13a59f7
krithiskkv <krithiskkv@gmail.com>
2016-09-27 12:00:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,21 @@
 //img.onclick = function() {
 //    var interval = setInterval(moveRight, 50);
 //};
+
+var button = document.getElementById('counter');
+button.onclick = function () {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET', 'http://krithiskkv.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
