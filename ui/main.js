84190c2504a5ff854b7aa692b3f3a7bce49e0045
krithiskkv <krithiskkv@gmail.com>
2016-09-27 11:38:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,16 @@
-console.log('Loaded!');
+// console.log('Loaded!');
 
 // var element = document.getElementById('main-text');
 // element.innerHTML = "New Value";
 
-var img = document.getElementById('prof');
-var marginLeft = 0;
+//var img = document.getElementById('prof');
+//var marginLeft = 0;
 
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
+//function moveRight() {
+//    marginLeft = marginLeft + 1;
+//    img.style.marginLeft = marginLeft + 'px';
+//}
 
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
-};
+//img.onclick = function() {
+//    var interval = setInterval(moveRight, 50);
+//};
