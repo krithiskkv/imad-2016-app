74186a7a6eb578d58eab0c40e8f1e4d0859863fb
krithiskkv <krithiskkv@gmail.com>
2016-09-28 11:20:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,13 +37,29 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    var names = ['names1', 'names2', 'names3'];
-    var list = '';
-    for (var i=0;i<names.length;i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+      var request = newXMLHttpRequest();
+      
+      request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+              //var names = ['names1', 'names2', 'names3'];
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0;i<names.length;i++) {
+                  list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+          }
+         }
+      }
+    
 };
+request.open('GET', 'http://krithiskkv.imad.hasura-app.io/counter', true);
+request.send(null);
+
+
+
 
 
