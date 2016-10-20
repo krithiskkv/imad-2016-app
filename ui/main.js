d67770855566d45bfb6b5ef74f89cfb8ca5ecfd7
krithiskkv <krithiskkv@gmail.com>
2016-10-20 10:12:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,7 +52,7 @@ button.onclick = function () {
             if (request.status === 200) {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-                span.innerHTML = initcounter.toString();
+                span.innerHTML = counter.toString();
             }
         }
     };
