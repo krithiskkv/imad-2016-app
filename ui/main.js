bce66d6c05ea559fcce483cef8c38bfe87df01ae
krithiskkv <krithiskkv@gmail.com>
2016-10-21 11:11:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -79,6 +79,7 @@ button.onclick = function () {
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     // console.log(articleName);
+    document.getElementById('name').value="";
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
