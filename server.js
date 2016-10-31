81984cfe177539c3ad189b2b8a69bfe090897efe
krithiskkv <krithiskkv@gmail.com>
2016-10-31 16:45:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,6 +129,7 @@ app.get('/test-db', function (req,res) {
 
 app.get('/articles/:articleName', function (req, res) {
       var articleName = req.params.articleName; 
+      console.log(articleName);
       pool.query("SELECT title, heading, date, content FROM article WHERE articlename = '" + articleName + "'", function(err,result) {
         if (err) {
            res.status(500).send(err.toString());
