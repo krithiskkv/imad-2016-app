f698518eb3265c3223ea298566b0a9ac52555c17
krithiskkv <krithiskkv@gmail.com>
2016-11-01 11:39:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,24 +74,6 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 
-//select data needed to build the page requested from the database and render it uing the createTemplate function
-app.get('/:articleName', function (req, res) {
-      var articleName = req.params.articleName; 
-      pool.query("SELECT * FROM article WHERE articlename=$1", [req.params.articleName], function(err,result) {
-        if (err) {
-           res.status(500).send(err.toString());
-        } else {
-             if (result.rows.length === 0) {
-                res.status(404).send('Article not found');
-            } else {
-                var articleData = result.rows[0];
-                res.send(createTemplate(articleData));
-            }
-        }
-     }); 
- });
- 
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -181,6 +163,24 @@ app.get('/submit-name4', function(req, res) {
     names4.push(name4);
     res.send(JSON.stringify(names4));
 });
+
+//select data needed to build the page requested from the database and render it uing the createTemplate function
+app.get('/:articleName', function (req, res) {
+      var articleName = req.params.articleName; 
+      pool.query("SELECT * FROM article WHERE articlename=$1", [req.params.articleName], function(err,result) {
+        if (err) {
+           res.status(500).send(err.toString());
+        } else {
+             if (result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+     }); 
+ });
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
