7fa41e505e82a73016e98da970f38340b2934759
krithiskkv <krithiskkv@gmail.com>
2016-11-01 11:07:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get('/articles/:articleName', function (req, res) {
              if (result.rows.length === 0) {
                 res.status(404).send('Article not found');
             } else {
-                var articleData = JSON.stringify(result.rows[0]);
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
         }
