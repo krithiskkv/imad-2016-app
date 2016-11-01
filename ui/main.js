313258909fb0f0997694e1ffab9544e65054de1b
krithiskkv <krithiskkv@gmail.com>
2016-11-01 12:24:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ switch ((document.getElementById('heading').innerHTML).trim()) {
         case "Home Page":
               initrequest.open('GET', 'http://krithiskkv.imad.hasura-app.io/initcounter1', true);
               break;
-        case "My Favourite Authors":
+        case "My Favorite Authors":
               initrequest.open('GET', 'http://krithiskkv.imad.hasura-app.io/initcounter2', true);
               break;
         case "Programming Experience":
