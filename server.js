d6fee9d5724ca467ad0cbe3168befeb15701e70b
krithiskkv <krithiskkv@gmail.com>
2016-11-05 17:20:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user:'krithiskkv',
@@ -67,9 +68,19 @@ function createTemplate (data) {
             return htmlTemplate;
 }
 
+function hash(input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/hash/:input', function(req, res) {
+    var hashedString = hash(req.params.input, 'random-string' );
+    res.send(hashedString); 
 });
 
 var pool = new Pool(config);
