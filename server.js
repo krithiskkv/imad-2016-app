35e6707bf05cc73475d6adbbd122d029e0de1f14
krithiskkv <krithiskkv@gmail.com>
2016-11-08 17:09:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,10 +135,17 @@ app.post('/login', function(req, res) {
 
 app.get('/check-login', function(req, res) {
    if (req.session && req.session.auth && req.session.auth.userId ) {
-       res.send('You are logged in' + req.session.auth.userId.toString());
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err,result) {
+           if (err) {
+               res.status(500).send('An error occured');
+           }
+           else {
+               res.send(result.rows[0].username);
+           }
+       });
    }
    else {
-       res.send('You are not logged in');
+       res.status(400).send('You are not logged in');
    }
 });
 
