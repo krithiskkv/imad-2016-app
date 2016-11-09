ce3ad7d76e2f3ac1740a44363edcce8cf7bc9649
krithiskkv <krithiskkv@gmail.com>
2016-11-09 11:11:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,23 +24,24 @@ function buildLogin() {
     loginarea.innerHTML = `<input type=text id = username placeholder=username /> <input type=password id = password placeholder = password /> Existing user? <button id=Login>Login</button> New user? <button id=Register>Register</button>`;
     var button2 = document.getElementById('Login');
     button2.onclick = function () {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                alert('Login successful');
-            }
-            else if (request.status === 403) {
-                    alert('Username/password does not exist');
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    alert('Login successful');
+                }
+                else if (request.status === 403) {
+                        alert('Username/password does not exist');
+                        document.getElementById('username').value = '';
+                        document.getElementById('password').value = '';
+                }
+                else if (request.status === 500) {
+                    alert('An error occured');
                     document.getElementById('username').value = '';
                     document.getElementById('password').value = '';
+                }
             }
-            else if (request.status === 500) {
-                alert('An error occured');
-                document.getElementById('username').value = '';
-                document.getElementById('password').value = '';
-            }
-        }
+        };
     };
 
     var username = document.getElementById('username').value;
@@ -51,28 +52,26 @@ function buildLogin() {
     
     var button3 = document.getElementById('Register');
     button3.onclick = function () {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                alert('User ' + username + ' created successfully');
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    alert('User ' + username + ' created successfully');
+                }
+                else if (request.status === 500) {
+                    alert('An error occured');
+                    document.getElementById('username').value = '';
+                    document.getElementById('password').value = '';
+                }
             }
-            else if (request.status === 500) {
-                alert('An error occured');
-                document.getElementById('username').value = '';
-                document.getElementById('password').value = '';
-            }
-        }
+        };
+    
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        request.open('POST', 'http://krithiskkv.imad.hasura-app.io/create-user', true);
+        request.setRequestHeader('Content-Type','application/json');
+        request.send(JSON.stringify({username : username, password: password}));
     };
-
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    request.open('POST', 'http://krithiskkv.imad.hasura-app.io/create-user', true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username : username, password: password}));
-};
-};
-
 }
 
 //build logout form
