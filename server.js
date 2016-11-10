d7ca3bbad000534c0580052b6490a37b4d6ea0e6
krithiskkv <krithiskkv@gmail.com>
2016-11-10 18:23:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -210,9 +210,10 @@ app.get('/counter/:articleName', function(req, res) {
 
 // /initcmnt obtains the current list of comments for a page from the comment table
 
-var comments = [];
+
 app.get('/initcmnt/:articleName', function(req, res) {
     pool.query("SELECT comment FROM article AS a, comment AS b WHERE articlename = $1 AND article_id = id ORDER BY b.date DESC, b.time DESC", [req.params.articleName], function(err, result) {
+        var comments = [];
         if (err) {
             res.status(500).send(err.toString()); }
         else { 
