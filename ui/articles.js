f3f850d99cc51470d077bab387c9aaa45568d49f
krithiskkv <krithiskkv@gmail.com>
2016-11-11 10:53:31 +0530

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -34,6 +34,52 @@ console.log(articleName);
 initcommrequest.open('GET', 'http://krithiskkv.imad.hasura-app.io/initcmnt/' + articleName, true);
 initcommrequest.send(null);
 
+var checkrequest = new XMLHttpRequest();
+checkrequest.onreadystatechange = function() {
+    if (checkrequest.readyState === XMLHttpRequest.DONE) {
+        if (checkrequest.status === 200) {
+            var cmntarea = `
+                Write a comment...
+                <br/>
+                <textarea rows="2" cols="50" class="scrollabletextbox" id="comment" name="comments" ></textarea>
+                <br/>
+                <input type="submit" id="submit_btn" value="Submit"> </input>`;
+            document.getElementById('cmntinput').innerHTML = cmntarea;                
+            //on clicking Submit button, add the text in the comment box to the database and display the updated comments list 
+            var submit = document.getElementById('submit_btn');
+            submit.onclick = function() {
+                var commInput = document.getElementById('comment');
+                var comment = commInput.value;
+                if (comment > " ") {
+                    var request = new XMLHttpRequest();
+                    request.onreadystatechange = function () {
+                        if (request.readyState === XMLHttpRequest.DONE) {
+                        if (request.status === 200) {
+                            var comments = request.responseText;
+                            comments = JSON.parse(comments);
+                            var list = '';
+                            for (var i=0;i<comments.length;i++) {
+                              list += '<li>' + names[i] + '</li>';
+                            }
+                            var ul = document.getElementById('namelist');
+                            ul.innerHTML = list;
+                            }
+                        }
+                    };
+                
+                    request.open('POST', 'http://krithiskkv.imad.hasura-app.io/submit-cmnt/' + articleName, true);
+                    request.setRequestHeader('Content-Type','application/json');
+                    request.send(JSON.stringify({comment : comment}));
+                    document.getElementById('comment').value="";
+                }
+            };
+        }    
+    }
+};
+
+checkrequest.open('GET', 'http://krithiskkv.imad.hasura-app.io/check-login', true);
+checkrequest.send(null);
+
 
 //on clicking Like button request server to increment Like counter and render the new Like counter
 var button1 = document.getElementById('counter');
@@ -53,32 +99,7 @@ button1.onclick = function () {
     request.send(null);
 };
 
-//on clicking Submit button, add the text in the comment box to the database and display the updated comments list 
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    var commInput = document.getElementById('comment');
-    var comment = commInput.value;
-    if (comment > " ") {
-        var request = new XMLHttpRequest();
 
-        request.onreadystatechange = function () {
-            if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                var comments = request.responseText;
-                comments = JSON.parse(comments);
-                var list = '';
-                for (var i=0;i<comments.length;i++) {
-                  list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
-                }
-            }
-        };
-    
-        request.open('POST', 'http://krithiskkv.imad.hasura-app.io/submit-cmnt/' + articleName, true);
-        request.setRequestHeader('Content-Type','application/json');
-        request.send(JSON.stringify({comment : comment}));
-        document.getElementById('comment').value="";
-    }
+
+
 };
