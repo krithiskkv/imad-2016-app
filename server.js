1f2392cfed571e7f0f1d6b8c1d1764ceef8e8fa3
krithiskkv <krithiskkv@gmail.com>
2016-11-11 17:32:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -270,15 +270,15 @@ app.post('/submit-cmnt/:articleName', function(req, res) {
 });
 
 app.get('/get-articles/:category', function (req, res) {
-      pool.query("SELECT * FROM article WHERE articlename=$1", [req.params.articleName], function(err,result) {
+      pool.query("SELECT * FROM article WHERE category=$1 ORDER BY date DESC", [req.params.category], function(err,result) {
         if (err) {
            res.status(500).send(err.toString());
         } else {
              if (result.rows.length === 0) {
-                res.status(404).send('Article not found');
+                res.status(404).send('Article category not found');
             } else {
                 var articleData = result.rows[0];
-                res.send(createTemplate(articleData));
+                res.send(JSON.stringify(result.rows));
             }
         }
      }); 
