948cc1c72bf565bc0eb902908cfeeadfbbcfe0c0
krithiskkv <krithiskkv@gmail.com>
2016-11-21 14:20:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ function createTemplate (data) {
             <meta name = "viewport" content = "width=device-width, initial-scale=1" />
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
-        <body background={$bgimage}>
+        <body id="body">
             <h3 id="heading">Krithika's BlogApp</h3>
             <div id="header"> 
                     <div id="articles">
