275aa49133f0331bf2e87198533aedc7d9a1ee46
krithiskkv <krithiskkv@gmail.com>
2016-11-21 12:34:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ function createTemplate (data) {
             <meta name = "viewport" content = "width=device-width, initial-scale=1" />
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
-        <body background="/ui/office.jpg">
+        <body background={$bgimage}>
             <h3 id="heading">Krithika's BlogApp</h3>
             <div id="header"> 
                     <div id="articles">
