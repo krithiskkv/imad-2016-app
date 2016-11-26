d51e2923e622a8f7a8c0607db28929b14fe39b7d
krithiskkv <krithiskkv@gmail.com>
2016-11-26 08:32:19 +0530

    [imad-console] Updates ui/post.js

--- ui/post.js
+++ ui/post.js
@@ -1,7 +1,7 @@
 var submit = document.getElementById('subarticle_btn');
 submit.onclick = function() {
     var request = new XMLHttpRequest();
-    var heading   = document.getElementById("heading");
+    var heading   = document.getElementById("artheading");
     var title     = document.getElementById("title").value;
     var shortname = document.getElementById("shortname").value;
     var category  = document.getElementById("category").value;
