856c98c190a4a3b40317ab4699f8f4d5c5938059
krithiskkv <krithiskkv@gmail.com>
2016-11-28 12:00:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ function createTemplate (data) {
                     <a id="cmntlink" href="#comments"> ${cmntcnt} comments </a>
                 </div>
                 <div>
-                    <div> '"' ${content} '"' <div>
+                    <div> '"' ${content} '"' </div>
                 </div>
                 <div id="comments">  
                     <hr/>
